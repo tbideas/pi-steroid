5073cd72762b4ff0f75f9f1bc05a8ab508bce3f8
Thomas Sarlandie <thomas@sarlandie.net>
2013-03-29 19:08:18 +0000

    Use forever to background process

--- pi-steroid-boot.sh
+++ pi-steroid-boot.sh
@@ -11,112 +11,46 @@
 ### END INIT INFO
 
 # Author: Thomas Sarlandie <thomas@sarlandie.net>
+# Heavily relies on npm module forever to daemonize/kill node scripts
 
 PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/bin
 DESC="Client for pijs.io cloud platform"
 NAME=pi-steroid
 DAEMON=/usr/local/bin/$NAME
-DAEMON_ARGS="pijs.io 80"
+DAEMON_ARGS="-s pijs.io -p 80"
 SCRIPTNAME=/etc/init.d/$NAME
+FOREVER=/usr/local/bin/forever
+
 
 # Exit if the package is not installed
 [ -x "$DAEMON" ] || exit 0
 
-# Read configuration variable file if it is present
-[ -r /etc/default/$NAME ] && . /etc/default/$NAME
-
-# Load the VERBOSE setting and other rcS variables
-. /lib/init/vars.sh
-
-# Define LSB log_* functions.
-# Depend on lsb-base (>= 3.2-14) to ensure that this file is present
-# and status_of_proc is working.
-. /lib/lsb/init-functions
-
-#
-# Function that starts the daemon/service
-#
-do_start()
-{
-  # Return
-  #   0 if daemon has been started
-  #   1 if daemon was already running
-  #   2 if daemon could not be started
-  start-stop-daemon --start --quiet --exec $DAEMON --test > /dev/null \
-    || return 1
-  start-stop-daemon --start --quiet --exec $DAEMON -b -- \
-    $DAEMON_ARGS \
-    || return 2
-}
-
-#
-# Function that stops the daemon/service
-#
-do_stop()
-{
-  # Return
-  #   0 if daemon has been stopped
-  #   1 if daemon was already stopped
-  #   2 if daemon could not be stopped
-  #   other if a failure occurred
-  start-stop-daemon --stop --quiet --retry=TERM/30/KILL/5 --name $NAME
-  RETVAL="$?"
-  [ "$RETVAL" = 2 ] && return 2
-  # Wait for children to finish too if this is a daemon that forks
-  # and if the daemon is only ever run from this initscript.
-  # If the above conditions are not satisfied then add some other code
-  # that waits for the process to drop all resources that could be
-  # needed by services started subsequently.  A last resort is to
-  # sleep for some time.
-  start-stop-daemon --stop --quiet --oknodo --retry=0/30/KILL/5 --exec $DAEMON
-  [ "$?" = 2 ] && return 2
-  return "$RETVAL"
-}
+# Exit if forever is not installed
+[ -x $FOREVER ] || exit 0
 
 case "$1" in
   start)
-  [ "$VERBOSE" != no ] && log_daemon_msg "Starting $DESC" "$NAME"
-  do_start
-  case "$?" in
-    0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
-    2) [ "$VERBOSE" != no ] && log_end_msg 1 ;;
-  esac
+    echo "piJS.io: Checking and installing updates for pi-steroid ..."
+    npm -g update pi-steroid
+    echo "piJS.io: Launching pi-steroid"
+    $FOREVER start $DAEMON $DAEMON_ARGS
   ;;
   stop)
-  [ "$VERBOSE" != no ] && log_daemon_msg "Stopping $DESC" "$NAME"
-  do_stop
-  case "$?" in
-    0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
-    2) [ "$VERBOSE" != no ] && log_end_msg 1 ;;
-  esac
+    $FOREVER stop $DAEMON
   ;;
   status)
-  status_of_proc "$DAEMON" "$NAME" && exit 0 || exit $?
+    $FOREVER list
   ;;
-  restart|force-reload)
-  #
-  # If the "reload" option is implemented then remove the
-  # 'force-reload' alias
-  #
-  log_daemon_msg "Restarting $DESC" "$NAME"
-  do_stop
-  case "$?" in
-    0|1)
-    do_start
-    case "$?" in
-      0) log_end_msg 0 ;;
-      1) log_end_msg 1 ;; # Old process is still running
-      *) log_end_msg 1 ;; # Failed to start
-    esac
-    ;;
-    *)
-    # Failed to stop
-    log_end_msg 1
-    ;;
-  esac
+  update)
+    echo "piJS.io: Checking and installing updates for pi-steroid ..."
+    npm -g update pi-steroid
   ;;
+  logs)
+    $FOREVER logs
+  ;;
+
   *)
-  echo "Usage: $SCRIPTNAME {start|stop|status|restart|force-reload}" >&2
+  echo "Usage: $SCRIPTNAME {start|stop|update|logs}" >&2
   exit 3
   ;;
 esac
